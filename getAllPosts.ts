437d3a68ef2e17311488acf2fc3ad133a8bbede2
Alan Harper <475+aussiegeek@users.noreply.github.com>
2021-02-13 16:35:08 +1100

    Cleanup mdx handling
    Ended up needing to deal with some assumptions about how posts are
    loaded, skipping webpack entirely.
    
    This meant stopping use of context.require and using straight node APIs
    instead.

--- getAllPosts.ts
+++ getAllPosts.ts
@@ -1,41 +1,29 @@
 import matter from "gray-matter";
 import * as fs from "fs";
+import * as path from "path";
+import renderToString from "next-mdx-remote/render-to-string";
 
-function importAll(r: __WebpackModuleApi.RequireContext) {
-  return r.keys().map((fileName) => ({
-    link: fileName.substr(1).replace(/\/index\.mdx$/, ""),
-    module: r(fileName),
-  }));
-}
+const postsDirectory = path.join(process.cwd(), "posts");
 
-export const posts = importAll(
-  require.context("./pages/posts/", true, /\.mdx$/)
-);
-
-export async function getAllPosts() {
-  const context = require.context("./posts", true);
-  const posts = [];
-  for (const key of context.keys()) {
-    const post = key.slice(2);
-    const path = `./posts/${post}`;
-    const content = fs.readFileSync(path, "utf-8");
-    const meta = matter(content);
-    posts.push({
-      slug: post.slice(0, -10),
-      title: meta.data.title,
-    });
-  }
-
-  return posts;
-}
+export const getAllPostIds = () => {
+  const fileNames = fs.readdirSync(postsDirectory);
+  return fileNames.map((fileName) => fileName.replace(/\.md$/, "")).reverse();
+};
 
 export async function getPostBySlug(slug: string | string[] | undefined) {
   const path = `./posts/${slug}/index.mdx`;
 
-  const meta = matter(fs.readFileSync(path, "utf-8"));
-  const { data, content } = matter(meta.content);
+  const { data, content } = matter(fs.readFileSync(path, "utf-8"));
+
+  const mdxSource = await renderToString(content);
+
   return {
-    title: meta.data.title,
-    content: content,
+    title: data.title,
+    slug,
+    mdxSource,
   };
 }
+
+export const getAllPosts = async () => {
+  return Promise.all(getAllPostIds().map((slug) => getPostBySlug(slug)));
+};
--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -4,24 +4,29 @@ import Link from "next/link";
 import Head from "next/head";
 import { GetStaticProps } from "next";
 import ConvertKitForm from "convertkit-react";
+import { MdxRemote } from "next-mdx-remote/types";
+import hydrate from "next-mdx-remote/hydrate";
 
-const Page: (props: { content: string; title: string }) => JSX.Element = ({
-  content,
-  title,
-}) => (
-  <>
-    <article className="mt-5 prose prose-lg">
-      <Head>
-        <title>{title}</title>
-      </Head>
-      <h1>{title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: content }} />
-    </article>
-    <div className="m-5 w-full">
-      <ConvertKitForm formId={1673792} template="mills" />
-    </div>
-  </>
-);
+const Page: (props: {
+  mdxSource: MdxRemote.Source;
+  title: string;
+}) => JSX.Element = ({ mdxSource, title }) => {
+  const content = hydrate(mdxSource);
+  return (
+    <>
+      <article className="mt-5 prose prose-lg">
+        <Head>
+          <title>{title}</title>
+        </Head>
+        <h1>{title}</h1>
+        {content}
+      </article>
+      <div className="m-5 w-full">
+        <ConvertKitForm formId={1673792} template="mills" />
+      </div>
+    </>
+  );
+};
 
 export default Page;
 
