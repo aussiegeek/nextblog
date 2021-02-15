0c664e01dfc179f188eae3610cb493e9a4309f46
Alan Harper <475+aussiegeek@users.noreply.github.com>
2021-02-15 17:48:56 +1100

    Add error handling for getAllPosts

--- getAllPosts.ts
+++ getAllPosts.ts
@@ -2,6 +2,13 @@ import matter from "gray-matter";
 import * as fs from "fs";
 import * as path from "path";
 import renderToString from "next-mdx-remote/render-to-string";
+import { MdxRemote } from "next-mdx-remote/types";
+
+export interface Post {
+  title: string;
+  slug: string;
+  mdxSource: MdxRemote.Source;
+}
 
 const postsDirectory = path.join(process.cwd(), "posts");
 
@@ -10,7 +17,12 @@ export const getAllPostIds = () => {
   return fileNames.map((fileName) => fileName.replace(/\.md$/, "")).reverse();
 };
 
-export async function getPostBySlug(slug: string | string[] | undefined) {
+export async function getPostBySlug(
+  slug: string | string[] | undefined
+): Promise<Post> {
+  if (typeof slug !== "string") {
+    throw "Post slug isn't a string";
+  }
   const path = `./posts/${slug}/index.mdx`;
 
   const { data, content } = matter(fs.readFileSync(path, "utf-8"));
@@ -18,7 +30,7 @@ export async function getPostBySlug(slug: string | string[] | undefined) {
   const mdxSource = await renderToString(content);
 
   return {
-    title: data.title,
+    title: data.title ?? "Untitled",
     slug,
     mdxSource,
   };
