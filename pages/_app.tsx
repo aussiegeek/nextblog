d23bdcd9ad81e67a58abe9837c6fdf7b018390ee
Alan Harper <475+aussiegeek@users.noreply.github.com>
2021-02-16 21:35:35 +1100

    Setup panelbear

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,10 @@
 import type { AppProps } from "next/app";
 import "tailwindcss/tailwind.css";
 import Link from "next/link";
+import { usePanelbear } from "../lib/panelbear";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  usePanelbear(process.env.PANELBEAR_SITE_ID);
   return (
     <div className="mt-10 max-w-4xl mx-auto">
       <div className="font-sans text-3xl">
